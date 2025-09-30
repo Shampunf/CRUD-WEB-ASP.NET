50627f1c280a409cc6d51ba8e8d205a65f541413
Shampunf <romancitahov@gmail.com>
2025-09-30 09:03:05 +0300

    чето

---
+++ BookStore/BookStore.DataAccess/Configurations/BookConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using BookStore.DataAccess.Entites;
+
+namespace BookStore.DataAccess.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<BookEntity>
+    {
+        public void Configure(EntityTypeBuilder<BookEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(b => b.Title)
+                .IsRequired()
+                .HasMaxLength(Book.MAX_TITLE_LENGTH);
+
+            builder.Property(b => b.Description)
+                .IsRequired();
+
+            builder.Property(b => b.Price)
+                .IsRequired();
+
+        }
+    }
+}
---
+++ BookStore/BookStore.DataAccess/Repositories/BooksRepository.cs
@@ -0,0 +1,62 @@
+﻿using BookStore.DataAccess.Entites;
+using BookStore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DataAccess.Repositories
+{
+    public class BooksRepository : IBooksRepository
+    {
+        private readonly BookStoreDbContext _context;
+
+        public BooksRepository(BookStoreDbContext context) { 
+            _context = context;
+        }
+
+        public async Task<List<Book>> Get ()
+        {
+            var bookEntities = await _context.Books
+                .AsNoTracking()
+                .ToListAsync();
+
+            var books = bookEntities
+                .Select(be => Book.Create(be.Id, be.Title, be.Description, be.Price).Book)
+                .ToList();
+            return books;
+        }
+
+        public async Task<Guid> Create(Book book)
+        {
+            var bookEntity = new BookEntity
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Description = book.Description,
+                Price = book.Price
+            };
+
+            await _context.Books.AddAsync(bookEntity);
+            await _context.SaveChangesAsync();
+
+            return bookEntity.Id;
+        }
+
+        public async Task<Guid> Update(Guid id, string title, string description, decimal price)
+        {
+            await _context.Books
+                .Where(b => b.Id == id)
+                .ExecuteUpdateAsync(s => s
+                    .SetProperty(b => b.Title, b => title)
+                    .SetProperty(b => b.Description, description)
+                    .SetProperty(b => b.Price, b => price));
+            return id;
+        }
+
+        public async Task<Guid> Delete(Guid id)
+        {
+            await _context.Books
+                .Where (b => b.Id == id)
+                .ExecuteDeleteAsync();
+            return id;
+        }
+    }
+}
