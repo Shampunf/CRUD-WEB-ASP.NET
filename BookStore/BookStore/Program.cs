015c3e7659a919c9baaed1d02bc8b58e34ceb922
Shampunf <romancitahov@gmail.com>
2025-10-04 19:08:44 +0300

    fhnjsg

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -1,3 +1,6 @@
+using BookStore.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -5,6 +8,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<BookStoreDbContext>(
+    options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(BookStoreDbContext)));
+    });
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
